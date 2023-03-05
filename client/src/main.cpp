02b55d485181a3392572e0fb9f700bfd6d5c9a11
sssi111 <gooday358@gmail.com>
2023-03-06 00:41:54 +0300

    fix client/main

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -4,25 +4,25 @@
 #include "client.hpp"
 #include "game.hpp"
 
-//enum class request_type { SendOk, ReceiveGameBoard, AddToLobby };
+// enum class request_type { SendOk, ReceiveGameBoard, AddToLobby };
 //
-//struct Request {
-//    request_type type;
-//    grpc::ClientContext context;
-//};
+// struct Request {
+//     request_type type;
+//     grpc::ClientContext context;
+// };
 //
-//void run_request(Client *client, const Request &request) {
-//            if (request.type == request_type::SendOk) {
-//                client->getStub()->SendOk();
-//            }
-//            else if (request.type == request_type::ReceiveGameBoard) {
-//                client->getStub()->AsyncReceiveGameBoard();
-//            } else if (request.type == request_type::AddToLobby){
-//                client->getStub()->AddToLobby();
-//            }
-//            grpc::ServerContext *context;
-//     client->getStub()->CallClient(context, Ser );
-//}
+// void run_request(Client *client, const Request &request) {
+//             if (request.type == request_type::SendOk) {
+//                 client->getStub()->SendOk();
+//             }
+//             else if (request.type == request_type::ReceiveGameBoard) {
+//                 client->getStub()->AsyncReceiveGameBoard();
+//             } else if (request.type == request_type::AddToLobby){
+//                 client->getStub()->AddToLobby();
+//             }
+//             grpc::ServerContext *context;
+//      client->getStub()->CallClient(context, Ser );
+// }
 
 int main(int argc, char *argv[]) {
     auto channel = grpc::CreateChannel(
@@ -32,12 +32,12 @@ int main(int argc, char *argv[]) {
     std::unique_ptr<Client> client = std::make_unique<Client>(channel);
     grpc::ClientContext context{};
     namespace_proto::GameState game_state;
+    namespace_proto::GameIdAndHero game_id_and_hero;
     std::mutex mutex;
-    std::thread([&client, &context, &mutex, &game_state]() {
-        google::protobuf::Empty google_empty;
+    std::thread([&client, &context, &mutex, &game_state, &game_id_and_hero]() {
         namespace_proto::GameState response;
         std::unique_ptr<grpc::ClientReader<namespace_proto::GameState> > reader(
-            client->getStub()->CallServer(&context, google_empty)
+            client->getStub()->CallServer(&context, game_id_and_hero)
         );
         while (reader->Read(&response)) {
             std::unique_lock l{mutex};
