fd714fa772092c3258b098d4bb9433e0ab044069
sssi111 <gooday358@gmail.com>
2023-03-18 22:04:21 +0300

    move game_session class in separate header

--- server/model/include/server.hpp
+++ server/model/include/server.hpp
@@ -1,7 +1,6 @@
 #include <grpc/grpc.h>
 #include <grpcpp/server_builder.h>
 #include <atomic>
-#include <game_session.hpp>
 #include <iostream>
 #include <map>
 #include <memory>
@@ -10,79 +9,7 @@
 #include <thread>
 #include <utility>
 #include <vector>
-#include "TSqueue.hpp"
-#include "choose_interactor.hpp"
-#include "game.hpp"
-#include "move_interactor.hpp"
-#include "proto/all_protos/demo.grpc.pb.h"
-
-class Player final {
-    int id;
-    ::grpc::ServerWriter<namespace_proto::GameState> *stream;
-
-public:
-    Player(int id, ::grpc::ServerWriter<namespace_proto::GameState> *stream)
-        : id(id), stream(stream) {
-    }
-
-    [[nodiscard]] int get_id() const {
-        return id;
-    }
-
-    [[nodiscard]] ::grpc::ServerWriter<namespace_proto::GameState> *get_stream(
-    ) const {
-        return stream;
-    }
-};
-
-class GameSession {
-    Player first_player;
-    Player second_player;
-    std::unordered_map<int, TSQueue<namespace_proto::GameState>>
-        response_queues;
-    namespace_proto::GameState game_state;
-    game_model::game model_game;
-    interactors::mover move_unit;
-    interactors::chooser choose_unit;
-
-public:
-    GameSession(Player first_player_, Player second_player_)
-        : first_player(first_player_),
-          second_player(second_player_),
-          model_game(first_player_.get_id(), second_player_.get_id()),
-          move_unit(model_game),
-          choose_unit(model_game) {
-    }
-
-    Player get_first_player() const {
-        return first_player;
-    }
-
-    Player get_second_player() const {
-        return second_player;
-    }
-
-    std::unordered_map<int, TSQueue<namespace_proto::GameState>>
-        *get_response_queues() {
-        return &response_queues;
-    }
-
-    namespace_proto::GameState *get_game_state() {
-        return &game_state;
-    }
-
-    game_model::game *get_model_game() {
-        return &model_game;
-    }
-
-    interactors::mover *get_mover() {
-        return &move_unit;
-    }
-
-    interactors::chooser *get_chooser() {
-        return &choose_unit;
-    }
-};
+#include "game_session.hpp"
 
 class ServerState {
 public:
@@ -92,8 +19,6 @@ public:
 };
 
 ServerState *get_server_state();
-
-// todo include all interactors
 
 class ServerServices final : public ::namespace_proto::Server::Service {
     ::grpc::Status CallServer(
