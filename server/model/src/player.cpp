d1ed6d92b85eba0fd78368a70b311e7b8db726fc
minmise <dokhru@gmail.com>
2023-03-19 19:10:04 +0300

    style fix player.cpp

--- server/model/src/player.cpp
+++ server/model/src/player.cpp
@@ -12,11 +12,11 @@ unit &player::get_unit(int index) {
 
 void player::set_start_units(int player_index, const board &game_board) {
     coordinates board_size = game_board.get_size();
-    int x_size = board_size.get_row();
-    int y_size = board_size.get_column();
-    int y = (player_index == 0 ? 0 : y_size - 1);
-    for (int x = 0; x < x_size; ++x) {
-        m_units_list.emplace_back(coordinates{x, y}, player_index, x);
+    int row_amount = board_size.get_row();
+    int column_amount = board_size.get_column();
+    int column = (player_index == 0 ? 0 : column_amount - 1);
+    for (int row = 0; row < row_amount; ++row) {
+        m_units_list.emplace_back(coordinates{row, column}, player_index, row);
     }
 }
 }  // namespace game_model
