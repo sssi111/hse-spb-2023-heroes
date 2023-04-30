e8a8aab844a4f97b464de62a548604015a51e2a6
sssi111 <gooday358@gmail.com>
2023-04-30 23:24:01 +0300

    some minor fixes in client main

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -17,44 +17,6 @@ int main() {
         main_menu.update();
         main_menu.render();
     }
-//    //    get_client_state()->m_user = user;
-//    int type;
-//    std::cout << "select type 0 - for login / 1 - for sign up .....  ";
-//    std::cin >> type;
-//    if (type == 0){
-//        std::cout << "LogIn: enter your name and password:\n";
-//        std::string name, password;
-//        while (user.user().id() == -1) {
-//            std::cin >> name >> password;
-//            namespace_proto::LogInData request;
-//            request.set_name(name);
-//            request.set_password(password);
-//            grpc::ClientContext context{};
-//            get_client_state()->m_stub->LogIn(
-//                &context, request, user.mutable_user()
-//            );
-//            if (user.user().id() == -1){
-//                std::cout << "try again\n";
-//            }
-//        }
-//    }
-//    else{
-//        std::cout << "SignUp: enter your name and password:\n";
-//        std::string name, password;
-//        while (user.user().id() == -1) {
-//            std::cin >> name >> password;
-//            namespace_proto::LogInData request;
-//            request.set_name(name);
-//            request.set_password(password);
-//            grpc::ClientContext context{};
-//            get_client_state()->m_stub->SignUp(
-//                &context, request, user.mutable_user()
-//            );
-//            if (user.user().id() == -1){
-//                std::cout << "try again\n";
-//            }
-//        }
-//    }
     std::cout << get_client_state()->m_user.user().id() << '\n';
     std::thread receiver(&Client::run_receiver);
     while (!game_view::get_game_state()->get_window()->is_done()) {
