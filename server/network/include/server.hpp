ff4c0247b159f06919fe28f5737de1b26cfb463f
sssi111 <gooday358@gmail.com>
2023-05-02 23:58:26 +0300

    add demo attacker in move_unit

--- server/network/include/server.hpp
+++ server/network/include/server.hpp
@@ -87,6 +87,20 @@ class ServerServices final : public ::namespace_proto::Server::Service {
         cell2->set_column(temp_column);
     }
 
+    static void update_unit(
+        namespace_proto::Unit *unit,
+        const game_model::coordinates &coordinates,
+        GameSession *game_session_res
+    ) {
+        game_model::game *model_game = game_session_res->get_model_game();
+        game_model::cell cell = model_game->get_cell(coordinates);
+        game_model::unit model_unit =
+            model_game->get_player(cell.get_player_index())
+                ->get_unit(cell.get_unit_index());
+        unit->set_sum_of_health(model_unit.get_health());
+        unit->set_amount_unit(model_unit.get_number());
+    }
+
     ::grpc::Status MoveUnit(
         ::grpc::ServerContext *context,
         const ::namespace_proto::MoveFromTo *request,
@@ -101,25 +115,46 @@ class ServerServices final : public ::namespace_proto::Server::Service {
         namespace_proto::GameState *game_state_ref =
             game_session_ref->get_game_state();
         game_model::coordinates from(request->start());
-        game_model::coordinates to(request->finish());
-        (*game_session_ref->get_mover())(from, to);
-        swapUnits(
-            game_state_ref->mutable_game_cells(
-                request->start().row() * 10 + request->start().column()
-            ),
-            game_state_ref->mutable_game_cells(
-                request->finish().row() * 10 + request->finish().column()
-            )
+        namespace_proto::Cell *cell_from = game_state_ref->mutable_game_cells(
+            request->start().row() * 10 + request->start().column()
         );
-        if (request->user().user().id() !=
-            game_session_ref->get_first_player().get_id()) {
-            (*(game_session_ref->get_response_queues())
-            )[game_session_ref->get_first_player().get_id()]
-                .push(*game_state_ref);
+        game_model::coordinates to(request->finish());
+        namespace_proto::Cell *cell_to = game_state_ref->mutable_game_cells(
+            request->finish().row() * 10 + request->finish().column()
+        );
+        if (game_session_ref->get_model_game()->get_cell(to).get_unit_index() ==
+            -1) {
+            (*game_session_ref->get_mover())(from, to);
+            swapUnits(cell_from, cell_to);
+            if (request->user().user().id() !=
+                game_session_ref->get_first_player().get_id()) {
+                (*(game_session_ref->get_response_queues())
+                )[game_session_ref->get_first_player().get_id()]
+                    .push(*game_state_ref);
+            } else {
+                (*(game_session_ref->get_response_queues())
+                )[game_session_ref->get_second_player().get_id()]
+                    .push(*game_state_ref);
+            }
         } else {
-            (*(game_session_ref->get_response_queues())
-            )[game_session_ref->get_second_player().get_id()]
-                .push(*game_state_ref);
+            std::cout << "attack selected\n";
+            (*game_session_ref->get_attacker())(from, to);
+            if (game_session_ref->get_model_game()->get_cell(to).get_unit_index(
+                ) == -1) {
+                // delete to;
+                cell_to->set_allocated_unit(nullptr);
+                cell_to->set_is_unit(false);
+            } else {
+                update_unit(cell_to->mutable_unit(), to, game_session_ref);
+            }
+            if (game_session_ref->get_model_game()->get_cell(from).get_unit_index(
+                ) == -1) {
+                cell_to->set_allocated_unit(nullptr);
+                cell_from->set_is_unit(false);
+                // delete from;
+            } else {
+                update_unit(cell_from->mutable_unit(), from, game_session_ref);
+            }
         }
         *response = *game_state_ref;
         return ::grpc::Status::OK;
@@ -142,7 +177,7 @@ class ServerServices final : public ::namespace_proto::Server::Service {
             new_cell->set_column(cell.get().get_coordinates().get_column());
         }
         enable_cells = (*game_session_ref->get_attack_selecter()
-                        )(selected, request->user().user().id());
+        )(selected, request->user().user().id());
         for (auto cell : enable_cells) {
             namespace_proto::Cell *new_cell = response->add_cells();
             new_cell->set_row(cell.get().get_coordinates().get_row());
