693de8c9f9f577ded9dc183d13f0c53f4ca2b5d8
sssi111 <gooday358@gmail.com>
2023-05-03 20:33:28 +0300

    json preparation

--- server/model/src/unit.cpp
+++ server/model/src/unit.cpp
@@ -7,7 +7,7 @@ namespace game_model {
 
 unit::unit(int type) : m_type(type) {
     std::string filename = "unit_type" + std::to_string(type) + ".txt";
-    std::ifstream unit_type_file("../../server/model/unit_types/" + filename);
+    std::ifstream unit_type_file("model/unit_types/" + filename);
     assert(unit_type_file.is_open());
     unit_type_file >> m_number >> m_max_health >> m_damage >> m_attack_range >>
         m_movement_range >> m_weight;
